3e6eb356a8e7544387f7856cf48afaad0bae55aa
Conor <ConorP95@hotmail.co.uk>
2016-08-25 13:07:19 +0100

    Fixed player movement and Rotation. Made the rotation and movement speed defined by level designer

--- RainbowJam/Assets/Scripts/PlayerMovement2D.cs
+++ RainbowJam/Assets/Scripts/PlayerMovement2D.cs
@@ -4,8 +4,9 @@ using System.Collections;
 public class PlayerMovement2D : MonoBehaviour 
 {
 
-	float maxSpeed = 20.0f;
-	float rotSpeed = 180.0f;
+	public float maxSpeed = 20.0f;				// Sets the max speed for the player
+	public float rotSpeed = 180.0f;				// Sets the rotation speed for the player
+
 	// Use this for initialization
 	void Start () 
 	{
@@ -15,16 +16,17 @@ public class PlayerMovement2D : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-		//Rotate the Ship
+		//Rotate the player
 		Quaternion rot = transform.rotation;
 		float z = rot.eulerAngles.z;
 		z -= Input.GetAxis ("Horizontal") * rotSpeed * Time.deltaTime;
 		rot = Quaternion.Euler (0, 0, z);
 		transform.rotation = rot;
 
-		//Move the Ship
+		//Move the player
 		Vector3 pos = transform.position;
-		pos.y += Input.GetAxis ("Vertical") * maxSpeed * Time.deltaTime;
+		Vector3 velocity = new Vector3(0, Input.GetAxis ("Vertical") * maxSpeed * Time.deltaTime, 0);
+		pos += rot * velocity;
 		transform.position = pos;
 	}
 }
