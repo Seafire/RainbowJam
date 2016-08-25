4de8a8134e55796ebfc17bc6fabde48bb313932f
Conor <ConorP95@hotmail.co.uk>
2016-08-25 13:33:49 +0100

    Screen Bounderies implemented

--- RainbowJam/Assets/Scripts/PlayerMovement2D.cs
+++ RainbowJam/Assets/Scripts/PlayerMovement2D.cs
@@ -7,6 +7,8 @@ public class PlayerMovement2D : MonoBehaviour
 	public float maxSpeed = 20.0f;				// Sets the max speed for the player
 	public float rotSpeed = 180.0f;				// Sets the rotation speed for the player
 
+	float playerBoundsRad = 1.5f;				// To stop the player going off the screen
+
 	// Use this for initialization
 	void Start () 
 	{
@@ -27,6 +29,29 @@ public class PlayerMovement2D : MonoBehaviour
 		Vector3 pos = transform.position;
 		Vector3 velocity = new Vector3(0, Input.GetAxis ("Vertical") * maxSpeed * Time.deltaTime, 0);
 		pos += rot * velocity;
+
+		if (pos.y + playerBoundsRad > Camera.main.orthographicSize) 
+		{
+			pos.y = Camera.main.orthographicSize - playerBoundsRad;
+		}
+		if (pos.y - playerBoundsRad < -Camera.main.orthographicSize) 
+		{
+			pos.y = -Camera.main.orthographicSize + playerBoundsRad;
+		}
+
+		float screenRatio = (float)Screen.width / (float)Screen.height;
+		Debug.Log (screenRatio);
+		float cameraWidth = Camera.main.orthographicSize * screenRatio;
+
+		if (pos.x + playerBoundsRad > cameraWidth) 
+		{
+			pos.x = cameraWidth - playerBoundsRad;
+		}
+		if (pos.x - playerBoundsRad < -cameraWidth) 
+		{
+			pos.x = -cameraWidth + playerBoundsRad;
+		}
+
 		transform.position = pos;
 	}
 }
