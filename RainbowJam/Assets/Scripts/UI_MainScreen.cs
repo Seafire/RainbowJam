21d842c12e562247571269a27b2de635880ada8d
Conor <ConorP95@hotmail.co.uk>
2016-08-30 18:25:12 +0100

    Added in Basic Main Menu

---
+++ RainbowJam/Assets/Scripts/UI_MainScreen.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI_MainScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void StartGame()
+	{
+		Application.LoadLevel (1);
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+
+	}
+}
