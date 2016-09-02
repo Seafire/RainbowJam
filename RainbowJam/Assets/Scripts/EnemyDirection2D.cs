9691f64de61fb22d27309f55e390e26daae3fa5a
Conor <ConorP95@hotmail.co.uk>
2016-09-02 13:15:25 +0100

    Added in the enemy sprites and gave them the ability to flip depending on what side they are on

--- RainbowJam/Assets/Scripts/EnemyDirection2D.cs
+++ RainbowJam/Assets/Scripts/EnemyDirection2D.cs
@@ -7,6 +7,7 @@ public class EnemyDirection2D : MonoBehaviour
 	private Transform treePos;
 
 	public float rotSpeed = 180.0f;
+
 	//public GameObject[] respawnPoint;
 
 	// Use this for initialization
@@ -38,9 +39,47 @@ public class EnemyDirection2D : MonoBehaviour
 
 		Vector3 dir = treePos.position - transform.position;
 		dir.Normalize ();
+		float facing = dir.x;
+		FlipSprite (facing);
 		float zAngle = Mathf.Atan2 (dir.y, dir.x) * Mathf.Rad2Deg - 90;
 
 		transform.rotation = Quaternion.Euler (0, 0, zAngle);
 
 	}
+
+	public void FlipSprite(float facing)
+	{
+		// Check to see if input is disabled
+		
+		// If we are moving to the right.
+		if (facing > 0f) 
+		{
+			// Swapping the sprite around.
+			Vector3 scale = transform.localScale;
+			
+			// Reversing the scale.
+			scale.x = 1.0f;
+			
+			// Setting the new scale for the sprite.
+			transform.localScale = scale;
+		} 
+		// Otherwise, if we are moving to the left.
+		else if (facing < 0f) 
+		{
+			// Swapping the sprite around.
+			Vector3 scale = transform.localScale;
+			
+			// Reversing the scale.
+			scale.x = -1.0f;
+			
+			// Setting the new scale for the sprite.
+			transform.localScale = scale;
+		}
+	}
+
+	protected void Flip()
+	{
+
+	}
+
 }
