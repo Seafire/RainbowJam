8d637320f6c63f3f8322876d233b49ecab48cbe5
Conor <ConorP95@hotmail.co.uk>
2016-09-04 15:13:44 +0100

    Main menu done. Added in the animator for the Tree.

---
+++ RainbowJam/Assets/Scripts/Tree.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tree : MonoBehaviour 
+{
+	private Score score;
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () 
+	{
+		anim = gameObject.GetComponent<Animator> ();
+		score = gameObject.GetComponentInParent<Score> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (score.posScore == 200) 
+		{
+			anim.SetFloat("posScore", 200.0f);
+		}
+		else if (score.posScore == 300) 
+		{
+			anim.SetFloat("posScore", 300.0f);
+		}
+		else if (score.posScore == 400) 
+		{
+			anim.SetFloat("posScore", 400.0f);
+		}
+		else if (score.posScore == 500) 
+		{
+			anim.SetFloat("posScore", 500.0f);
+		}
+		else if (score.posScore == 600) 
+		{
+			anim.SetFloat("posScore", 600.0f);
+		}
+		else if (score.posScore == 700) 
+		{
+			anim.SetFloat("posScore", 700.0f);
+		}
+	}
+}
