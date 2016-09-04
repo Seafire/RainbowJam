29244457c4f13b2cd6ff74f362b6a2d706c5a545
Conor <ConorP95@hotmail.co.uk>
2016-09-04 22:13:16 +0100

    Added in RainDrops

--- RainbowJam/Assets/Scripts/DestroyBullet.cs
+++ RainbowJam/Assets/Scripts/DestroyBullet.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class DestroyBullet : MonoBehaviour 
 {
 	public float timer = 5.0f;
+	public enum bulletType {playerBullet, rainDrop};
+	public bulletType bullet;
 
 	// Use this for initialization
 	void Start () 
@@ -18,6 +20,25 @@ public class DestroyBullet : MonoBehaviour
 		if (timer <= 0.0f)
 		{
 			Destroy(gameObject);
+		}
+
+	}
+
+	void OnTriggerEnter2D(Collider2D coll)
+	{
+		if (coll.tag == "Enemy") 
+		{
+			if (bullet == bulletType.playerBullet) 
+			{
+				Destroy (gameObject);
+			}
+		}
+		if (coll.tag == "Player") 
+		{
+			if (bullet == bulletType.rainDrop) 
+			{
+				Destroy (gameObject);
+			}
 		}
 	}
 }
